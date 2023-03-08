3ae0f80552b46a46fb49b6c736cbffaff77e9b30
kinyarasam <skinyara.30@gmail.com>
2023-03-08 15:44:57 +0300

    Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,40 +1,58 @@
 #!/usr/bin/env node
-import redis from 'redis';
 /**
- * @module
+ * @module redis
  */
+import redis from 'redis';
+import util from 'util';
 
+/**
+ * A class representing a redis client
+ */
 class RedisClient {
-  constructor () {
+  constructor() {
     this.client = redis.createClient();
 
     // Display any errors on the console.
     this.client.on('error', (err) => {
-      console.log(err);
+      console.log(err.message);
+      this.connected = false;
+      return false;
+    });
+
+    this.client.on('connect', () => {
+      this.connected = true;
+      return true;
     });
   }
 
-  async isAlive() {
-    const promise = (resolve) => {
-      this.client.ping((err, reply) => {
-        if (err) {
-          resolve(false);
-        } else {
-          resolve(reply === 'PONG');
-        }
-      });
+  // Check the connection status and report
+  isAlive() {
+    if (this.client) {
+      return true;
     }
-
-    return new Promise(promise);
+    return false;
   }
 
-  get() {}
+  // get value for given key from redis server
+  async get(key) {
+    const redisGet = util.promisify(this.client.get).bind(this.client);
+    const value = await redisGet(key);
+    return value;
+  }
 
-  set () {}
+  // set key value pair for redis server
+  async set(key, value, time) {
+    const redisSet = util.promisify(this.client.setex).bind(this.client);
+    return redisSet(key, time, value);
+  }
 
-  del () {}
+  // del key value pair from redis server.
+  async del(key) {
+    const redisDel = util.promisify(this.client.del).bind(this.client);
+    await redisDel(key);
+  }
 }
 
-const redisClient = new RedisClient;
+const redisClient = new RedisClient();
 
 module.exports = redisClient;
