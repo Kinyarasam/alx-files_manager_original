08f7a687abc1e8abf4bbebf816747641515f98a4
kinyarasam <skinyara.30@gmail.com>
2023-03-09 12:00:59 +0300

    Authenticate a user

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const express = require('express');
 
 const router = express.Router();
 
 // GET
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
