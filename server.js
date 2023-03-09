702ac088ad4eb07c076db273eeaee4ba0e752873
kinyarasam <skinyara.30@gmail.com>
2023-03-09 09:53:27 +0300

    Added json middleware

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+/**
+ * @middleware
+ */
+app.use(express.json())
 app.use('/', routes);
 
 const start = () => {
